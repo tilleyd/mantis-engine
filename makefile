c5a02e0e3d7a82da9924b43ba997ea77dfc15268
Duncan Tilley <duncan.tilley@gmail.com>
2017-10-30 14:52:03 +0200

    Add interval timer

--- makefile
+++ makefile
@@ -12,11 +12,20 @@ OFILES = $(BIN)/demo.o $(BIN)/exception.o $(BIN)/window.o
 demo: $(BIN) $(OFILES)
 	$(CC) $(CFLAGS) -o demo $(OFILES) $(LIBS)
 
+# DEMO
+
 $(BIN)/demo.o: $(SRC)/demo.cpp $(SRC)/mantis.h
 	$(CC) -c $(CFLAGS) -o $(BIN)/demo.o $(SRC)/demo.cpp
+
+# MANTIS_H
 
 $(BIN)/exception.o: $(SRC)/exception.cpp $(SRC)/mantis.h
 	$(CC) -c $(CFLAGS) -o $(BIN)/exception.o $(SRC)/exception.cpp
 
 $(BIN)/window.o: $(SRC)/window.cpp $(SRC)/mantis.h
 	$(CC) -c $(CFLAGS) -o $(BIN)/window.o $(SRC)/window.cpp
+
+# MANTIS_TIMING_H
+
+$(BIN)/interval.o: $(SRC)/interval.cpp $(SRC)/mantis_timing.h
+	$(CC) -c $(CFLAGS) -o $(BIN)/interval.o $(SRC)
---
+++ src/interval.cpp
@@ -0,0 +1,61 @@
+/*==============================================================================
+ * ME_Interval timer implementation
+ *     Author  : Duncan Tilley
+ *     Modified: 2017 Oct 30
+ *============================================================================*/
+
+#include "mantis.h"
+#include "mantis_timing.h"
+#include <SDL2/SDL.h>
+
+#define MAX_FRAME_SKIPS 8
+
+ME_Interval::ME_Interval(unsigned int freq)
+	: _freq(freq)
+	, _running(false)
+{}
+
+void ME_Interval::start(ME_IntervalObserver* obs)
+{
+	if (obs == NULL) {
+		throw ME_Exception("IntervalObserver is NULL");
+	}
+	_running = true;
+	// calculate the expected period
+	unsigned int period = 1000 / _freq;
+	double dperiod = 1.0 / _freq;
+
+	unsigned int startTime = SDL_GetTicks();
+	unsigned int before = startTime;
+	unsigned int missed = 0;
+	int oversleep = 0;
+	int sleep;
+	unsigned int after, skips;
+
+	while (_running) {
+		obs->timestep(this, dperiod);
+		after = SDL_GetTicks();
+		sleep = period - after - oversleep;
+		if (sleep > 0) {
+			// blocks for the remaining time
+			SDL_Delay(sleep);
+			// check oversleep caused by OS scheduling
+			oversleep = SDL_GetTicks() - after - sleep;
+		} else {
+			// add the missed sleep time
+			missed -= sleep;
+		}
+		// catch up missed frames
+		skips = 0;
+		while (missed >= period && skips < MAX_SKIPS) {
+			++skips;
+			missed -= period;
+		}
+		before = SDL_GetTicks();
+	}
+}
+
+void ME_Interval::stop()
+{
+	_running = false;
+}
