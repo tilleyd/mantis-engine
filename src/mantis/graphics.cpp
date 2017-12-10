c0c4aff1b3f44148ae331c34e83b5a5462779291
Duncan Tilley <duncan.tilley@gmail.com>
2017-12-10 22:10:56 +0200

    Add GNU license preamble

--- src/mantis/graphics.cpp
+++ src/mantis/graphics.cpp
@@ -1,5 +1,20 @@
 /*==============================================================================
  * ME_Graphics implementation
+ *==============================================================================
+ * This file is part of Mantis Engine.
+ *
+ * Mantis Engine is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * Mantis Engine is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Mantis Engine.  If not, see <http://www.gnu.org/licenses/>.
  *============================================================================*/
 
 #include "mantis.h"
